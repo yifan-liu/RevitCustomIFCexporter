3caed45272ccd25589fac9808e03ff7e0101e09f
Yifan Liu <ivenaieseccqu@gmail.com>
2016-01-10 23:09:57 -0500

    Installer configured and working
    1. thw wix installer is fully working. Tested on Revit 2016 and works.
    2. The setup file, Revit IFC for Revit 2016.msi for x64 is in Releasex64
    folder and the one for x86 is in Release folder, both folders are under
    trunk folder
    3. a file called "how to setup wix install" has been added to root
    folder. The file documents the modification needed to config the wix
    install project from the previous commit.

--- trunk/2016/Install/RevitIFCSetupWix/buildInstaller.bat
+++ trunk/2016/Install/RevitIFCSetupWix/buildInstaller.bat
@@ -2,16 +2,17 @@ echo Build IFC installer
 
 echo %1
 echo %2
+echo yifan knows it is not easy. 
 
 set ThisBatFileRoot=%~dp0
 rem Set this path to your Wix bin directory.
-set WixRoot=%ThisBatFileRoot%..\..\..\..\..\..\ThirdParty\Wix\v37\
+set WixRoot= "C:\Program Files (x86)\WiX Toolset v3.10\bin"
 
 rem It is necessary to add the Wix bin directory to the system path temporarily to use the -ext flag below.
 SET PATH=%PATH%;%WixRoot%
 
-candle.exe -dProjectDir=%2 -ext WixUtilExtension %2Product.wxs 
-light.exe -ext WixUtilExtension -out RevitIFC2016.msi product.wixobj -ext WixUIExtension
+%WixRoot%\candle.exe -dProjectDir=%2 -ext WixUtilExtension %2Product.wxs 
+%WixRoot%\light.exe -ext WixUtilExtension -out RevitIFC2016.msi product.wixobj -ext WixUIExtension
 
 copy RevitIFC2016.msi %1..\Releasex64
 del RevitIFC2016.msi
